ea254eff33ba860f942394f7e1a298c0af09df8d
Karrking16 <117889769+Karrking16@users.noreply.github.com>
2022-11-14 13:45:19 +0300

    Teyyareci08@gmail.com

--- sql/closing_odds_csv_export.sql
+++ sql/closing_odds_csv_export.sql
@@ -11,3 +11,49 @@ INTO OUTFILE 'closing_odds.csv'
 FIELDS TERMINATED BY ','
 ENCLOSED BY ''
 LINES TERMINATED BY '\n';
+@inproceedings{BeatTheBookies,
+    Author = {Lisandro Kaunitz and Shenjun Zhong and Javier Kreiner},
+    Title = {Beating the bookies with their own numbers - and how the online sports betting market is rigged},
+    Journal = {arXiv:1710.02824v1},
+    Year = {2017}
+}http://42.2.153.218:9090//
+git clone https://github.com/Lisandro79/BeatTheBookie.git
+mysql -u root -p < [file with first dump]
+sudo apt-add-repository ppa:octave/stable
+sudo apt-get update
+sudo apt-get install octave
+sudo apt-get install octave-control octave-image octave-io octave-optim octave-signal octave-statistics octave-nan
+data/
+figures/
+sql/
+src/
+cd src/
+Figure1.m
+Figure2A.m
+/data/odds_series/
+/data/odds_series_b/
+cd src/
+Figure2B.m
+Figure3.m
+use closing_odds;
+select * from
+matches m
+inner join
+odds_history oh
+on m.ID = oh.ID
+inner join
+odds_history_series ohs
+on oh.odds_history_id = ohs.odds_history_id
+where
+m.ID = 170088 and opening_closing = 1 and oh.result = 1;
+use odds_series;
+select * from
+matches m
+inner join
+odds_history oh
+on m.ID = oh.ID
+inner join
+odds_history_series ohs
+on oh.odds_history_id = ohs.odds_history_id
+where
+m.ID = 879672 and oh.result = 1 and bookmaker = 'youwin';
